83fce02ac9bc58c9c7808959d0543b2290d09c34
iv <consmebius@gmail.com>
2020-11-08 20:46:53 +0200

    .

--- django_server_HaloLab/settings.py
+++ django_server_HaloLab/settings.py
@@ -134,15 +134,15 @@ AUTH_PASSWORD_VALIDATORS = [
 ]
 
 # Cache Redis
-CACHES = {
-    "default": {
-        "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
-        "OPTIONS": {
-            "CLIENT_CLASS": "django_redis.client.DefaultClient",
-        }
-    }
-}
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#         }
+#     }
+# }
 SESSION_ENGINE = "django.contrib.sessions.backends.cache"
 SESSION_CACHE_ALIAS = "default"
 CACHE_TTL = 15 * 1
