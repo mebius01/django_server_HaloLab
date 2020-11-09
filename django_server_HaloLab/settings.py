bfc5ec87593eb883a1e7bdb9306c0c494b457aef
iv <consmebius@gmail.com>
2020-11-09 14:32:35 +0200

    fix ettings..

--- django_server_HaloLab/settings.py
+++ django_server_HaloLab/settings.py
@@ -174,14 +174,8 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
 
 # django-debug-toolbar
 INTERNAL_IPS = ('127.0.0.1',)
